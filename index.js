1b44d8a1a939aea6dd2d51d6204368e26dcedc8c
GradusFrontend <ruslandjavadov998@gmail.com>
2023-11-09 22:20:00 +0500

    09.11.23 homework guilties / legit

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-// 08.11.23
+let people = [
+    {
+        name: "bobur",
+        age: 43,
+        isMarried: false,
+        wifes: ['kamila', 'safir', 'allayor']
+    },
+    {
+        name: "ismail",
+        age: 12,
+        isMarried: true,
+        wifes: ['nikita', 'malika']
+    },
+    {
+        name: "allayor",
+        age: 14,
+        isMarried: true,
+        wifes: ['denis', 'dima', 'dimon', 'xbegim']
+    },
+    {
+        name: "sarodr ml",
+        age: 18,
+        isMarried: false,
+        wifes: ['safiya']
+    },
+]
 
-let attempts = 3
+let guilties = []
+let legit = []
 
-while(attempts > 0) {
-    let pwd = +prompt('password')
-
-    if(pwd === 7777) {
-        alert('welcome')
-        break
+for(let person of people) {
+    if(person.wifes.length > 1 || person.age < 18) {
+        guilties.push(person)
     } else {
-        attempts--
-        alert(`incorrect! attempts left : ${attempts}`)
+        legit.push(person)
     }
+}
 
-    if(attempts === 0) {
-        alert('attempts are over')
-    }
+
+for(let person of guilties) {
+    document.write('GUILTY:' + '<br>' + ' Name: ' + person.name + '<br>' + ' Age: ' + person.age + '<br>' + ' Wifes: ' + person.wifes + '<br>' + '<br>')
+}
+
+for(let person of legit) {
+    document.write('LEGIT:' + '<br>' + ' Name: ' + person.name + '<br>' + ' Age: ' + person.age + '<br>' + ' Wifes: ' + person.wifes + '<br>' + '<br>')
 }
