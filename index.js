6cb3c1196af74263e419a56d1586cc23dd37868d
GradusFrontend <ruslandjavadov998@gmail.com>
2023-11-15 14:44:52 +0500

    15.11.23 homework courses / count

--- index.js
+++ index.js
@@ -1,46 +1,134 @@
-let people = [
+let arr = [
     {
-        name: "bobur",
-        age: 43,
-        isMarried: false,
-        wifes: ['kamila', 'safir', 'allayor']
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
     },
     {
-        name: "ismail",
-        age: 12,
-        isMarried: true,
-        wifes: ['nikita', 'malika']
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
     },
     {
-        name: "allayor",
-        age: 14,
-        isMarried: true,
-        wifes: ['denis', 'dima', 'dimon', 'xbegim']
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
     },
     {
-        name: "sarodr ml",
-        age: 18,
-        isMarried: false,
-        wifes: ['safiya']
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+     {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
     },
 ]
 
-let guilties = []
-let legit = []
 
-for(let person of people) {
-    if(person.wifes.length > 1 || person.age < 18) {
-        guilties.push(person)
-    } else {
-        legit.push(person)
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {       
+        course: '   BACKEND',
+        count: 0
+    },
+]
+
+
+for(let i = 0; i < arr.length; i++) { 
+    for(let k = 0; k < categories.length; k++) {
+       
+        if(arr[i].info.faculity.trim().toLowerCase() === categories[k].course.trim().toLowerCase()) {
+            categories[k].count++
+            categories[k].names += arr[i].name
+        }
     }
 }
-
-
-for(let person of guilties) {
-    document.write('GUILTY:' + '<br>' + ' Name: ' + person.name + '<br>' + ' Age: ' + person.age + '<br>' + ' Wifes: ' + person.wifes + '<br>' + '<br>')
-}
-
-for(let person of legit) {
-    document.write('LEGIT:' + '<br>' + ' Name: ' + person.name + '<br>' + ' Age: ' + person.age + '<br>' + ' Wifes: ' + person.wifes + '<br>' + '<br>')
-}
+console.table(categories)
