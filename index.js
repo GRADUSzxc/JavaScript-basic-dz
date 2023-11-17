bb866538be76883c857ac7164c9b385ebc000e01
GradusFrontend <ruslandjavadov998@gmail.com>
2023-11-17 16:36:02 +0500

    17.11.23 homework Compaines

--- index.js
+++ index.js
@@ -1,134 +1,119 @@
-let arr = [
+let companies = [
     {
         id: Math.random(),
-        name: 'Timur',
-        info: {
-            school: '235',
-            faculity: 'SMM'
-        },
-    },
-    {
-        id: Math.random(),
-        name: 'Imran',
-        info: {
-            school: 'ne izvestno',
-            faculity: 'programming'
-        },
+        name: "wepro",
+        tax: 12,
+        budget: 10000,
+        expensesPerYear: [4000, 2000, 3000] 
     },
     {
         id: Math.random(),
-        name: 'Aminjon',
-        info: {
-            school: '444',
-            faculity: 'Dizayn'
-        },
+        name: 'Allayor',
+        tax: 12,
+        budget: 200,
+        expensesPerYear: [150, 300, 700]
     },
     {
         id: Math.random(),
-        name: 'Maxmud',
-        info: {
-            school: '777',
-            faculity: '3dsmax'
-        },
+        name: "Daler-Xrust",
+        tax: 10,
+        budget: 50000,
+        expensesPerYear: [12000, 20000, 5000]
     },
     {
         id: Math.random(),
-        name: 'Muxammad',
-        info: {
-            school: '5555',
-            faculity: 'Backend'
-        },
+        name: 'Luchshiy Productoviy',
+        tax: 20,
+        budget: 23000,
+        expensesPerYear: [2000, 1000, 5000]
     },
     {
         id: Math.random(),
-        name: 'Timur',
-        info: {
-            school: '235',
-            faculity: 'SMM'
-        },
+        name: "dilrukh salon",
+        tax: 4,
+        budget: 32000,
+        expensesPerYear: [12000, 10000, 10000]
     },
     {
         id: Math.random(),
-        name: 'Imran',
-        info: {
-            school: 'ne izvestno',
-            faculity: 'programming'
-        },
+        name: "Necromantiya",
+        tax: 10,
+        budget: 30000,
+        expensesPerYear: [1200,3444,4666]
+    },
+    {
+        id:Math.random(),
+        name:'Artyomida.uz',
+        tax:15,
+        budget:100000,
+        expensesPerYear:[10000,3000,90000]
     },
     {
         id: Math.random(),
-        name: 'Aminjon',
-        info: {
-            school: '444',
-            faculity: 'Dizayn'
-        },
+        name: "bmw",
+        tax: 13, 
+        budget: 13000, 
+        expensesPerYear: [3000, 4000, 1000]
     },
     {
         id: Math.random(),
-        name: 'Maxmud',
-        info: {
-            school: '777',
-            faculity: '3dsmax'
-        },
+        name: "dizayner",
+        tax: 12,
+        budget: 20000,
+        expensesPerYear: [1000, 100, 200]
     },
-     {
-        id: Math.random(),
-        name: 'Maxmud',
-        info: {
-            school: '777',
-            faculity: '3dsmax'
-        },
+    {
+        id:Math.random(),
+        name: 'Amerika_cherez_mexika',
+        tax: 0,
+        budget: 15000,
+        expensesPerYear: [100,4000]
     },
     {
         id: Math.random(),
-        name: 'Muxammad',
-        info: {
-            school: '5555',
-            faculity: 'Backend'
-        },
+        name: 'Gradus MMM',
+        tax: 12,
+        budget: 250000,
+        expensesPerYear: [20000, 50000, 15000]
+    },
+    {
+        id:Math.random(),
+        name:"Tangir Company",
+        tax: 100,
+        budget: 1000000,
+        expensesPerYear: [10000,12000,20000]
     },
     {
         id: Math.random(),
-        name: 'Muxammad',
-        info: {
-            school: '5555',
-            faculity: 'Backend'
-        },
-    },
+        name: "film_for_adults",
+        tax: 75,
+        budget: 50000,
+        expensesPerYear: [15000, 10000, 5000]
+    }
 ]
+let unprofitable = []
+let zero = []
+let profitable = []
 
 
-let categories = [
-    {
-        course: ' SMM',
-        count: 0
-    },
-    {
-        course: 'PROGRAMMING',
-        count: 0
-    },
-    {
-        course: '     3DSMAX',
-        count: 0
-    },
-    {
-        course: ' DIZAYN',
-        count: 0
-    },
-    {       
-        course: '   BACKEND',
-        count: 0
-    },
-]
+for(let item of companies) {
+    item.expensesPerMonth = 0
+    for(let nums of item.expensesPerYear) {
+        item.expensesPerMonth += nums / 12
+    }
+    let taxSum = item.tax * (item.budget / 12) / 100
+    
+    item.total = Math.round((item.budget / 12) - (item.expensesPerMonth + taxSum))
 
-
-for(let i = 0; i < arr.length; i++) { 
-    for(let k = 0; k < categories.length; k++) {
-       
-        if(arr[i].info.faculity.trim().toLowerCase() === categories[k].course.trim().toLowerCase()) {
-            categories[k].count++
-            categories[k].names += arr[i].name
-        }
+    if(item.total > 0) {
+        profitable.push(item)
+    } else if (item.total < 0 ) {
+        unprofitable.push(item)
+    } else {
+        zero.push(item)
     }
 }
-console.table(categories)
+
+console.log('ПРИБЫЛЬНЫЕ: ', profitable)
+console.log('УБЫТОЧНЫЕ: ', unprofitable)
+console.log('В НОЛЬ: ', zero)
