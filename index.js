0d4c03b804c740518ba34d7d53ca7005ecfed746
GradusFrontend <ruslandjavadov998@gmail.com>
2023-11-22 17:57:01 +0500

    21.11.23 homework random Id / Custom Sort

--- index.js
+++ index.js
@@ -1,93 +1,40 @@
+// 21.11.23
 
-// 19.11.23
-   // ex1
-
-function checkAge(age) {
-    if(age >= 18) {
-        alert('welcome')
-    } else if(age > 0 && age < 18) {
-        alert('you are small')
-    } else if(age !== 'number' || age < 1) {
-        alert('goofball!')
+function randomID(length) {
+    let ID = ''
+    let letters = 'qwertyuiopasdfghjklzxcvbnm1234567890'
+    for(let i = 0; i < length; i++) {
+        ID += letters.charAt(Math.round(Math.random() * letters.length))
     }
+    alert(ID)
 }
 
-checkAge(prompt('age'))
+randomID(10)
 
 
 
-    // ex2
+// ex2
 
-function maxName(name, name1, name2) {
-    let max = Math.max(name.length, name1.length, name2.length) 
-    if(name.length === max) {
-        alert(name)
-    } else if(name1.length === max) {
-        alert(name1)
-    } else if(name2.length === max) {
-        alert(name2)
+function sortArray(arr) {
+    let sorted = false
+    while (sorted === false) {
+      sorted = true
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          let temp = arr[i]
+          arr[i] = arr[i + 1]
+          arr[i + 1] = temp
+          sorted = false
+        }
+      }
     }
-}
+    return arr
+  }
+  
+  
+  let sortedArr = sortArray([9,7,6,8,5,3,4])
+  console.log(sortedArr)
+  
 
-maxName('alex', 'george', 'Michael')
-
-
-
-// ex3
-function min(a, b) {
-    if(a && b >= 1) {
-        alert(Math.min(a, b))
-    } else if(Array.isArray(a)) {
-
-        let minNumArr = a[0]
-
-        for(let i = 1; i < a.length; i++) {
-            if(a[i] < minNumArr) {
-                minNumArr = a[i]
-            }
-        }
-        alert(minNumArr)
-    } else if(typeof(a) === 'object') {
-
-        let arrForObj = Object.values(a)
-        let minNumArr1 = arrForObj[0]
-
-        for(let i = 1; i < arrForObj.length; i++) {
-            if(arrForObj[i] < minNumArr1) {
-                minNumArr1 = arrForObj[i]
-            }
-        }
-        alert(minNumArr1)
-
-    } else if(typeof(a) === 'object' && typeof(b) === 'object') {
-
-        let concat = Object.values(a)
-        let concat1 =  Object.values(b)
-        let arrForObj1 = concat.concat(concat1)
-
-        let minNumArr2 = arrForObj1[0]
-
-        for(let i = 1; i < arrForObj1.length; i++) {
-            if(arrForObj1[i] < minNumArr2) {
-                minNumArr2 = arrForObj1[i]
-            }
-        }
-        alert(minNumArr2)
-    }
-}
-min(1, 2)
-min([1, 2])
-min({a: 1, b: 2})
-min({a: 1, b: 2}, {a: 11, b: 12})
-
-
-
-// ex4
-
-function findProportion(total, broken) {
-    alert('total: ' + total + ' ' + 'broken: ' + broken + ' ' + '= ' + broken / total * 100 + '%')
-}
-
-findProportion(100000, 66666)
 
 
