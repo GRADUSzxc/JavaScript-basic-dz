4e4313740b947043de14c72061b56aa2066ae84f
GradusFrontend <ruslandjavadov998@gmail.com>
2023-11-23 16:37:11 +0500

    change 21.11.23

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function sortArray(arr) {
       sorted = true
       for (let i = 0; i < arr.length - 1; i++) {
         if (arr[i] > arr[i + 1]) {
-          let temp = arr[i]
+          let vremenniy = arr[i]
           arr[i] = arr[i + 1]
-          arr[i + 1] = temp
+          arr[i + 1] = vremenniy
           sorted = false
         }
       }
